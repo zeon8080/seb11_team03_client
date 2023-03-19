b69060240114f87f56d5a15ee1d101eb209f8a1f
DumakIt <odumak4411@gmail.com>
2023-03-19 19:56:48 +0900

    [Design]_#23_subLocationSelector_UI

---
+++ src/components/subLocationSelector/subLocationSelectorStyles.ts
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+export const SelectList = styled.div`
+  width: 230px;
+  height: 470px;
+  padding: 20px;
+  background: #ffffff;
+  border: 1px solid #bdbdbd;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  overflow: auto;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  & > div:last-of-type {
+    border-bottom: 1px solid #e0e0e0;
+  }
+`;
+
+export const Location = styled.div`
+  width: 100%;
+  height: 40px;
+  border: 1px solid #e0e0e0;
+  border-bottom: none;
+  text-align: center;
+  line-height: 40px;
+  font-weight: 500;
+  font-size: 14px;
+
+  &:hover {
+    border: 2px solid #fbb240;
+    color: #fbb240;
+  }
+`;
