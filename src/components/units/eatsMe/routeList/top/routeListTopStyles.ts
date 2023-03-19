cca542c58f5119a2186d7d77225cf739d45322ea
DumakIt <odumak4411@gmail.com>
2023-03-19 20:42:26 +0900

    [Design]_#24_routeList_UI

--- src/components/units/eatsMe/routeList/top/routeListTopStyles.ts
+++ src/components/units/eatsMe/routeList/top/routeListTopStyles.ts
@@ -5,17 +5,49 @@ export const Container = styled.header`
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
+  position: relative;
   height: 100px;
   box-shadow: 0px 2px 6px 2px rgba(60, 64, 67, 0.15),
     0px 1px 2px rgba(60, 64, 67, 0.3);
   border-radius: 15px;
 `;
 
-export const LocationSelector = styled.div`
+export const LocationWrapper = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   width: 140px;
   height: 60px;
+  background: #f5f5f5;
+  border-radius: 10px;
 
-  border: 1px solid red;
+  & > div:first-of-type {
+    width: calc(100% - 34px);
+    text-align: center;
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  &:focus > div:nth-of-type(2) {
+    background-image: url("/arrow_up.webp");
+  }
+
+  & + div {
+    display: none;
+  }
+
+  &:focus + div {
+    display: block;
+    position: absolute;
+    top: 85px;
+    left: -20px;
+  }
+`;
+
+export const Arrow = styled.div`
+  width: 24px;
+  height: 24px;
+  background-image: url("/arrow_down.webp");
 `;
 
 export const SearchBar = styled.div`
