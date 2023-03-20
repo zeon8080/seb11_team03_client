e5a8a4b68e22df8505e860db4a81d2e88b7f3a63
zeon8080 <jec0228@naver.com>
2023-03-20 16:06:54 +0900

    [Design]_#29_userInfo_pick_UI

--- src/components/units/eatsMe/userInfo/reserveList/middle/reserveListMiddleStyles.ts
+++ src/components/units/eatsMe/userInfo/reserveList/middle/reserveListMiddleStyles.ts
@@ -1,1 +1,105 @@
+
 import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 684px;
+  margin-right:20px;
+  margin-bottom: 20px;
+`;
+
+export const Title = styled.span`
+  font-size: 35px;
+  font-weight: bold;
+  color: black;
+  margin-bottom: 100px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 774px;
+  height: 224px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  padding: 20px 36px;
+`;
+
+export const ReserveBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 30px;
+`;
+
+export const FoodImg = styled.img`
+  width: 184px;
+  height: 184px;
+  object-fit: contain;
+`;
+
+export const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+  margin-right: 8px;
+`;
+
+export const Align = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  font-size: 18px;
+  margin-bottom: 15px;
+`;
+
+export const ScheduleBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 500px;
+`;
+
+export const ScheduleCont = styled.div`
+  display: flex;
+`;
+
+export const Date = styled.span`
+  line-height: 40px;
+  text-align: center;
+  font-size: 16px;
+  width: 144px;
+  height: 40px;
+  background-color: #ff6b3f;
+  color: white;
+  border-radius: 5px;
+`;
+
+export const Time = styled.span`
+  line-height: 40px;
+  text-align: center;
+  font-size: 16px;
+  width: 62px;
+  height: 40px;
+  background-color: #ff6b3f;
+  color: white;
+  border-radius: 5px;
+  margin-left: 10px;
+`;
+
+export const EditBtn = styled.button`
+  all: unset;
+  line-height: 40px;
+  text-align: center;
+  font-size: 16px;
+  width: 70px;
+  height: 40px;
+  border-radius: 5px;
+  color: white;
+  background-color: black;
+  margin-left: 10px;
+  cursor: pointer;
+`;
--- src/components/units/eatsMe/userInfo/userInfoStyles.ts
+++ src/components/units/eatsMe/userInfo/userInfoStyles.ts
@@ -1,26 +1,42 @@
+
 import styled from "@emotion/styled";
 
-export const Tab = styled.div`
-  font-size: 18px;
-`;
-
-export const PickedList = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 780px;
+  box-shadow: 0px 2px 6px 2px rgba(60, 64, 67, 0.15), 0px 1px 2px rgba(60, 64, 67, 0.3);
+  border-radius: 15px;
 `;
 
-export const UserWriteList = styled.div`
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: left;
 `;
 
-export const ReserveList = styled.div`
+export const UserBox = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  width: 216px;
 `;
+
+export const UserImg = styled.img`
+  width: 168px;
+  height: 168px;
+  object-fit: contain;
+  cursor: pointer;
+`;
+
+export const SelectImg = styled.img`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  right: 24px;
+  bottom: 24px;
+`;
+
+
