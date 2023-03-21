d623f990830154052a30a56fb6b724d981b35545
zeon8080 <jec0228@naver.com>
2023-03-21 14:04:22 +0900

    [Design]_#31_popularList_UI

--- src/components/units/eatsMe/popularList/middle/popularListMiddle.tsx
+++ src/components/units/eatsMe/popularList/middle/popularListMiddle.tsx
@@ -3,32 +3,121 @@ import * as S from "./popularListMiddleStyles";
 export default function PopularListMiddle(): JSX.Element {
   return (
     <S.Container>
-      <header>지역1 지역2 맛집</header>
-      <S.Wrapper>
+      <S.ListWrapper>
         <S.StoreBox>
           <S.StoreCont>
+            <S.FoodImg src="/defaultfood.webp" />
             <div>
-              <S.FoodImg src="/defaultfood.webp" />
+              <S.RatingBox>
+                <S.Rating>4.6</S.Rating>
+                <S.Img src="/heart_fill.webp" alt="" />
+              </S.RatingBox>
+
+              <S.StoreName>
+                <S.Img src="/store.webp" />
+                <span>가나다라마바사아자차카</span>
+              </S.StoreName>
+              <span>월요일 휴무</span>
+              <S.TimeInfoBox>
+                <div>Open 11:00</div>
+                <div>Close 22:00</div>
+              </S.TimeInfoBox>
             </div>
+          </S.StoreCont>
+          <S.StoreInfoBox>
+            <div>
+              <S.Img src="/marker_or.webp" />
+              <span>서울특별시 구로구 디지털로 300 </span>
+            </div>
+            <div>
+              <S.Img src="/phone.webp" />
+              <span>02-123-4567</span>
+            </div>
+          </S.StoreInfoBox>
+        </S.StoreBox>
+        <S.StoreBox>
+          <S.StoreCont>
+            <S.FoodImg src="/defaultfood.webp" />
             <div>
               <S.Rating>4.6</S.Rating>
-              <img src="" />
-              <div>
-                <S.StoreImg src="/store.webp" />
-                <span>상호명</span>
-              </div>
-              <div>휴무</div>
-              <div>Open 11:00</div>
-              <div>Close 22:00</div>
+              <S.StoreName>
+                <S.Img src="/store.webp" />
+                <span>가나다라마바사아자차카</span>
+              </S.StoreName>
+              <span>월요일 휴무</span>
+              <S.TimeInfoBox>
+                <div>Open 11:00</div>
+                <div>Close 22:00</div>
+              </S.TimeInfoBox>
             </div>
-            {/* <img src="" />
-        <span>주소</span>
-        <img src="" />
-        <span>02-123-4567</span> */}
           </S.StoreCont>
+          <S.StoreInfoBox>
+            <div>
+              <S.Img src="/location_or_stroke.webp" />
+              <span>서울특별시 구로구 디지털로 300 </span>
+            </div>
+            <div>
+              <S.Img src="/phone.webp" />
+              <span>02-123-4567</span>
+            </div>
+          </S.StoreInfoBox>
         </S.StoreBox>
-        <S.MapBox>지도</S.MapBox>
-      </S.Wrapper>
+        <S.StoreBox>
+          <S.StoreCont>
+            <S.FoodImg src="/defaultfood.webp" />
+            <div>
+              <S.Rating>4.6</S.Rating>
+              <S.StoreName>
+                <S.Img src="/store.webp" />
+                <span>가나다라마바사아자차카</span>
+              </S.StoreName>
+              <span>월요일 휴무</span>
+              <S.TimeInfoBox>
+                <div>Open 11:00</div>
+                <div>Close 22:00</div>
+              </S.TimeInfoBox>
+            </div>
+          </S.StoreCont>
+          <S.StoreInfoBox>
+            <div>
+              <S.Img src="/location_or_stroke.webp" />
+              <span>서울특별시 구로구 디지털로 300 </span>
+            </div>
+            <div>
+              <S.Img src="/phone.webp" />
+              <span>02-123-4567</span>
+            </div>
+          </S.StoreInfoBox>
+        </S.StoreBox>
+        <S.StoreBox>
+          <S.StoreCont>
+            <S.FoodImg src="/defaultfood.webp" />
+            <div>
+              <S.Rating>4.6</S.Rating>
+              <S.StoreName>
+                <S.Img src="/store.webp" />
+                <span>가나다라마바사아자차카</span>
+              </S.StoreName>
+              <span>월요일 휴무</span>
+              <S.TimeInfoBox>
+                <div>Open 11:00</div>
+                <div>Close 22:00</div>
+              </S.TimeInfoBox>
+            </div>
+          </S.StoreCont>
+          <S.StoreInfoBox>
+            <div>
+              <S.Img src="/location_or_stroke.webp" />
+              <span>서울특별시 구로구 디지털로 300 </span>
+            </div>
+            <div>
+              <S.Img src="/phone.webp" />
+              <span>02-123-4567</span>
+            </div>
+          </S.StoreInfoBox>
+        </S.StoreBox>
+      </S.ListWrapper>
+      <S.MapBox>지도</S.MapBox>
     </S.Container>
   );
 }
--- src/components/units/eatsMe/popularList/middle/popularListMiddleStyles.ts
+++ src/components/units/eatsMe/popularList/middle/popularListMiddleStyles.ts
@@ -2,42 +2,56 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  gap: 20px;
   box-shadow: 0px 2px 6px 2px rgba(60, 64, 67, 0.15),
     0px 1px 2px rgba(60, 64, 67, 0.3);
   border-radius: 15px;
   background-color: white;
 `;
 
-export const Wrapper = styled.div`
+export const ListWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  width: 624px;
+  margin: 20px 0;
 `;
 
 export const StoreBox = styled.div`
   display: flex;
   flex-direction: column;
-  width: 624px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  width: 300px;
+  height: 180px;
+  padding: 4px;
 `;
 
 export const StoreCont = styled.div`
   display: flex;
   flex-direction: row;
-  width: 300px;
-  height: 150px;
-  background-color: #f5f5f5;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 160px;
+  }
+`;
+export const RatingBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Rating = styled.span`
-  font-size: 24px;
+  font-size: 30px;
   font-weight: bold;
   color: #fbb240;
-  text-shadow: -1px 0px #000000, 0px 1px #000000, 1px 0px #000000,
-    0px -1px #000000;
 `;
 
 export const FoodImg = styled.img`
@@ -47,14 +61,57 @@ export const FoodImg = styled.img`
   margin-right: 10px;
 `;
 
-export const StoreImg = styled.img`
-  width: 20px;
-  height: 20px;
+export const Img = styled.img`
+  width: 24px;
+  height: 24px;
   object-fit: contain;
+  margin-right: 4px;
+`;
+
+export const StoreName = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & > span {
+    line-height: 24px;
+    font-size: 18px;
+  }
+
+  & + span {
+    font-size: 12px;
+    color: #333333;
+  }
+`;
+
+export const TimeInfoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & > div {
+    font-size: 12px;
+    color: #333333;
+    margin-right: 18px;
+  }
+`;
+
+export const StoreInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    display: flex;
+    flex-direction: row;
+    line-height: 24px;
+
+    & > span {
+      font-size: 12px;
+    }
+  }
 `;
 
 export const MapBox = styled.div`
   width: 480px;
   height: 600px;
+  margin: 20px 0;
   background-color: gray;
 `;
