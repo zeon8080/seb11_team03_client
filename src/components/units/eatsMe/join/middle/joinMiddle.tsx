c7fc3fb43b6b59ad123d114ac333b700a4ca37e4
zeon8080 <jec0228@naver.com>
2023-03-23 18:28:52 +0900

    [Design]_#50_join_UI

--- src/components/units/eatsMe/join/middle/joinMiddle.tsx
+++ src/components/units/eatsMe/join/middle/joinMiddle.tsx
@@ -1,39 +1,121 @@
-export default function JoinMiddle(): JSX.Element {
-  return (
-    <>
-      <div>
-        <title></title>
-        <span>join us</span>
-        <div>divide라인</div>
-        <div>
-          <span>이메일</span>
-          <input type="text" />
-          <button>인증번호 보내기</button>
-          <div>
-            <span>인증코드입력해주세요</span>
-            <input type="text" />
-            <span>3:00</span>
-            <button>확인</button>
-            <span>인증번호 재전송하기</span>
-          </div>
-        </div>
-        <div>
-          <span>비밀번호</span>
-          <input type="password" />
-        </div>
-        <div>
-          <span>비밀번호 확인</span>
-          <input type="password" />
-        </div>
-        <span>닉네임</span>
-        <div>
-          <input type="text" />
-          <button>중복</button>
-        </div>
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as S from "./joinMiddleStyles";
+import { schema } from "./joinMidleValidation";
 
-        <button>회원가입</button>
-        <button>로그인하러가기</button>
-      </div>
-    </>
+// interface IJoinMiddleFormData {
+//   email: string;
+//   nickname: string;
+//   password: string;
+//   passwordCheck: string;
+// }
+
+export default function JoinMiddle(): JSX.Element {
+  const [time, setTime] = useState(10);
+  const [isStarted, setIsStarted] = useState(false);
+  const min = Math.floor(time / 60);
+  const sec = String(time % 60).padStart(2, "0");
+
+  const { register, formState, handleSubmit } = useForm<IJoinMiddleFormData>({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  useEffect(() => {
+    let timer;
+
+    if (isStarted) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+
+    if (time <= 0) {
+      clearInterval(timer);
+      setIsStarted(false);
+    }
+
+    return () => clearInterval(timer);
+  }, [isStarted, time]);
+
+  const onClickAuth = (): void => {
+    setIsStarted(true);
+  };
+
+  const onClickReAuth = (): void => {
+    setTime(5);
+    setIsStarted(false);
+    onClickAuth();
+  };
+
+  const onClickJoin = (): void => {};
+
+  return (
+    <form onSubmit={handleSubmit(onClickJoin)}>
+      <S.Container>
+        <S.Wrapper>
+          <S.Title>
+            <h1>회원가입</h1>
+            <span>join us</span>
+          </S.Title>
+
+          <S.DivideLine></S.DivideLine>
+          <S.EmailBox>
+            <span>이메일</span>
+            <input type="text" {...register("email")} />
+            <p>{formState.errors.email?.message}</p>
+            <button type="button" onClick={onClickAuth}>
+              인증번호 보내기
+            </button>
+            <S.AccreditBox>
+              <span>이메일로 전송된 인증코드를 입력해주세요.</span>
+              <S.TokenBox>
+                <input type="text" placeholder="인증코드 6자리 입력" />
+                <span id="timer">
+                  {min}:{sec}
+                </span>
+                <button
+                  type="button"
+                  // style={{
+                  //   backgroundColor: time === 0 ? "#a5a5a5" : "#fbb240",
+                  // }}
+                >
+                  확인
+                </button>
+              </S.TokenBox>
+
+              <button type="button" onClick={onClickReAuth}>
+                인증번호 재전송하기
+              </button>
+            </S.AccreditBox>
+          </S.EmailBox>
+          <S.PasswordBox>
+            <span>비밀번호</span>
+            <input type="password" {...register("password")} />
+            <p>{formState.errors.password?.message}</p>
+            <span>비밀번호 확인</span>
+            <input type="password" {...register("passwordCheck")} />
+            <p>{formState.errors.passwordCheck?.message}</p>
+          </S.PasswordBox>
+          <S.NicknameBox>
+            <span>닉네임</span>
+            <div>
+              <input type="text" {...register("nickname")} />
+              <button type="button">중복 확인</button>
+            </div>
+
+            <p>{formState.errors.nickname?.message}</p>
+          </S.NicknameBox>
+          <S.BtnBox>
+            <button type="button">회원가입</button>
+            <div>
+              이미 아이디가 있으신가요?
+              <button>로그인</button>
+            </div>
+          </S.BtnBox>
+        </S.Wrapper>
+      </S.Container>
+    </form>
   );
 }
---
+++ src/components/units/eatsMe/join/middle/joinMidleValidation.ts
@@ -0,0 +1,32 @@
+import * as yup from "yup";
+
+export const schema = yup.object({
+  email: yup
+    .string()
+    .email("이메일 형식이 아닙니다.")
+    .required("이메일은 필수입니다."),
+
+  nickname: yup
+    .string()
+    .max(8, "8글자 이내로 적어주세요")
+    .required("별칭을 입력해주세요."),
+  password: yup
+    .string()
+    .min(8, "8자리 이상 입력해주세요.")
+    .max(16, "16자리 이내로 입력해주세요.")
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}[^\s]*$/,
+      "영문+숫자 조합 8~16자리의 비밀번호를 입력해주세요."
+    )
+    .required("비밀번호를 입력해주세요."),
+
+  passwordCheck: yup
+    .string()
+    .min(8, "8자리 이상 입력해주세요.")
+    .max(16, "16자리 이내로 입력해주세요.")
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}[^\s]*$/,
+      "영문+숫자 조합 8~16자리의 비밀번호를 입력해주세요."
+    )
+    .required("비밀번호를 입력해주세요."),
+});
