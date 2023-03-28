e938c0e58a525daf3d1f61bfff67ae1ddb571e01
byoungsu kim <kimet2001@gmail.com>
2023-03-28 11:51:27 +0900

    [Design]_#81_reactive_UI

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -6,6 +6,11 @@ import LayoutHeader from "./header/layoutHeader";
 interface ILayoutPros {
   children: JSX.Element;
 }
+const breakpoints = [576, 768, 992, 1200];
+const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+// ${mq[0]} {
+//   font-size: 50px;
+// }
 
 const Container = styled.div`
   height: 100vh;
@@ -14,6 +19,9 @@ const Container = styled.div`
 const LayoutBody = styled.div`
   width: 1200px;
   margin: 30px auto 0;
+  ${mq[3]} {
+    width: 100%;
+  }
 `;
 
 export default function Layout(props: ILayoutPros): JSX.Element {
--- src/components/units/eatsMe/main/middle/mainMiddleStyles.ts
+++ src/components/units/eatsMe/main/middle/mainMiddleStyles.ts
@@ -1,15 +1,22 @@
 import styled from "@emotion/styled";
-
+const breakpoints = [576, 768, 992, 1200];
+const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+// ${mq[0]} {
+//   font-size: 50px;
+// }
 export const Container = styled.div`
   position: relative;
   color: black;
   margin: 0;
   overflow: hidden;
+  ${mq[3]} {
+    width: 100%;
+  }
 `;
 
 export const Wrapper = styled.div`
   height: calc(100vh - 100px);
-  width: 100vw;
+  width: 100%;
   transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1);
   will-change: transform;
 `;
