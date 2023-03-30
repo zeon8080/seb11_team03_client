0c24db4d93a158a149553d8407edf1ee71fa61af
zeon8080 <jec0228@naver.com>
2023-03-31 00:09:26 +0900

    test ec2

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   // swcMinify: true,
-  // generateBuildId: () => "eatsme",
+  generateBuildId: () => "eatsme",
 
-  // exportPathMap: () => ({
-  //   "/": { page: "/" },
-  //   "/routeWrite": { page: "/routeWrite" },
-  //   "/404": { page: "/404" },
-  // }),
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    // "/eatsMe": { page: "/eatsMe" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
