c8e556146d04238a4feaf74ddbb9b7af545a82fb
byoungsu kim <kimet2001@gmail.com>
2023-04-03 19:42:14 +0900

    [Feat]_#162_userInfo

--- src/components/units/eatsMe/userInfo/userInfo.tsx
+++ src/components/units/eatsMe/userInfo/userInfo.tsx
@@ -2,18 +2,62 @@ import { Tabs } from "antd";
 import * as S from "./userInfoStyles";
 import PickedList from "./pickedList/pickedList";
 import ReserveListMiddle from "./reserveList/middle/reserveListMiddle";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useMutationUploadFile } from "../../../commons/hooks/mutation/useMutationUploadFile";
+import { useMutationUpdateUser } from "../../../commons/hooks/mutation/useMutationUpdateUser";
 
 export default function UserInfo(): JSX.Element {
+  const [imgUrl, setImgUrl] = useState("");
+  const [file, setFile] = useState<File>();
+  const ImgRef = useRef<HTMLInputElement>(null);
+
+  const [updateUser] = useMutationUpdateUser();
+  const [uploadFile] = useMutationUploadFile();
+
+  const onFile = async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
+    const file = e.target.files?.[0];
+    if (file === undefined) return;
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = async (e) => {
+      if (typeof e.target?.result === "string") {
+        setImgUrl(e.target.result);
+        setFile(file);
+
+        const resultFile = await uploadFile({ variables: { file } });
+        console.log(resultFile.data?.uploadFile);
+        await updateUser({
+          variables: {
+            updateUserInput: {
+              userImg: resultFile.data?.uploadFile,
+            },
+          },
+        });
+        console.log("사진이 업로드되었습니다!");
+      }
+    };
+  };
+
+  const onClickImg = (): void => {
+    ImgRef.current?.click();
+  };
   return (
     <S.Container>
       <S.Wrapper>
         <S.Style tabPosition={"left"} defaultActiveKey="1">
           <Tabs.TabPane
             tab={
-              <S.Tab>
-                <S.UserImg src="/userImg.webp" />
-                <S.SelectImg src="/imgSelect.webp" />
-              </S.Tab>
+              <S.ImgWrap onClick={onClickImg}>
+                <S.UserImg src={imgUrl === "" ? "/userImg.webp" : imgUrl} />
+                <input
+                  type="file"
+                  ref={ImgRef}
+                  onChange={(e) => {
+                    void onFile(e);
+                  }}
+                />
+              </S.ImgWrap>
             }
             key="4"
             disabled
